aa4dad396dc97bc6c288577548279e468ff07604
Peter Goodman <peter.goodman@gmail.com>
2018-11-16 12:38:21 -0500

    Make vmill-execute use gflags

--- Execute.cpp
+++ Execute.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2018 Trail of Bits, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Support/ManagedStatic.h>
@@ -8,8 +27,18 @@
 #include "vmill/Program/Snapshot.h"
 #include "vmill/Program/AddressSpace.h"
 
-int main(int argc, char const * const * argv){
-  auto snapshot = vmill::LoadSnapshotFromFile("/home/sai/ToB/remill/tools/tlift/vmill/snapshot");
+int main(int argc, char **argv) {
+  std::stringstream ss;
+  ss << std::endl << std::endl
+     << "  " << argv[0] << " \\" << std::endl
+     << "    --workspace WORKSPACE_DIR \\" << std::endl
+     << "    ..." << std::endl;
+
+  google::InitGoogleLogging(argv[0]);
+  google::SetUsageMessage(ss.str());
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  auto snapshot = vmill::LoadSnapshotFromFile(vmill::Workspace::SnapshotPath());
   vmill::Executor executor;
   vmill::Workspace::LoadSnapshotIntoExecutor(snapshot, executor);
   executor.Run();
